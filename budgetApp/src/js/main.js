5e17105b4fc51217313a83cea14a35a122793c2a
Vitalik <panins899@gmail.com>
2021-02-23 20:39:18 +0300

    исправил

--- budgetApp/src/js/main.js
+++ budgetApp/src/js/main.js
@@ -92,7 +92,7 @@ let appData = {
 
         if (typeof(items) != "string" || items == "" || typeof(items) == null) {
             console.log("Вы ввели некорректные данные или не ввели их вовсе");
-        } els {
+        } else {
             appData.income = items.split(", ");
             appData.income.push(prompt("Может что-то еще?"));
             appData.income.sort();
