f50f530cd4218e00b6d7cf8f54623507f397a8ff
Vitalik <panins899@gmail.com>
2021-02-26 00:13:16 +0300

    начал верстать сайт

---
+++ project_1/src/js/main.js
@@ -0,0 +1,5 @@
+import '../../node_modules/bootstrap/js/src/util/index.js'
+import '../../node_modules/bootstrap/js/dist/alert.js'
+import '../../node_modules/bootstrap/js/dist/button.js'
+import '../../node_modules/bootstrap/js/dist/carousel.js'
+import '../../node_modules/bootstrap/js/dist/collapse.js'
