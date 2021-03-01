c46b1e7c4188b65910aaa6c6cd800b065e49e1cf
Vitalik <panins899@gmail.com>
2021-03-01 08:06:23 +0300

    делаю моб меню

--- project_1/src/js/main.js
+++ project_1/src/js/main.js
@@ -3,3 +3,11 @@ import '../../node_modules/bootstrap/js/dist/alert.js'
 import '../../node_modules/bootstrap/js/dist/button.js'
 import '../../node_modules/bootstrap/js/dist/carousel.js'
 import '../../node_modules/bootstrap/js/dist/collapse.js'
+
+let menu_btn = document.querySelector('.menu_btn');
+
+menu_btn.addEventListener("click", function()
+{
+    menu_btn.classList.toggle("change");
+   
+})
