3adb5d13fcfcd4da70bfcf43aa8b28bcf4badace
Vitalik <panins899@gmail.com>
2021-03-01 21:25:14 +0300

    Доделал мобилку

--- project_1/src/js/main.js
+++ project_1/src/js/main.js
@@ -1,13 +1,12 @@
-import '../../node_modules/bootstrap/js/src/util/index.js'
-import '../../node_modules/bootstrap/js/dist/alert.js'
-import '../../node_modules/bootstrap/js/dist/button.js'
-import '../../node_modules/bootstrap/js/dist/carousel.js'
-import '../../node_modules/bootstrap/js/dist/collapse.js'
-
 let menu_btn = document.querySelector('.menu_btn');
-
-menu_btn.addEventListener("click", function()
-{
-    menu_btn.classList.toggle("change");
-   
+let menu = document.querySelector('.menu');
+let btn = document.querySelector('button')
+console.log(btn);
+// menu_btn.addEventListener("click", function()
+// {  menu_btn.classList.toggle("change");
+//    menu.classList.toggle("menu_active");
+//    console.log(menu);
+// })
+btn.addEventListener('click', function(e){
+    console.log('Произошло событиz ' + e.type + " по элементу" + e.target)
 })
